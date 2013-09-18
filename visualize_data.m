8fb9336884499890d486622105d3d79a6fa94a17
AwesomeEvilGenius <aayushgupta05@gmail.com>
2013-09-18 16:06:47 -0700

    Create visualize_data.m

---
+++ visualize_data.m
@@ -0,0 +1,21 @@
+
+d = load('data.txt');
+
+f = d(:,1);
+rng = d(:,2);
+int = d(:,3);
+
+theta = [0 : 1 : 359]'*pi/180;
+
+x = rng.*cos(theta);
+y = rng.*sin(theta);
+
+%idxg = find(rng>0.6 & rng<6.0 & f==0);
+idxg = find(f==0);
+
+figure;
+hold on;
+plot(x,y,'.');
+plot(x(idxg),y(idxg),'r.');
+axis equal;
+grid on;
